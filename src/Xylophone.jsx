cd9c570018599f7f292d401a484941d39957bad4
mehtaReena <j.reena07@gmail.com>
2021-05-01 21:51:28 +0530

    commit

--- src/Xylophone.jsx
+++ src/Xylophone.jsx
@@ -7,7 +7,7 @@ export default function Xylophone(props) {
 
      return(
          <div className ='container'>
-
+          <h2> Xylophone</h2>
          <div className='xylophone'>
              {
                  notes.map(note=><Bar note={note}></Bar>)
